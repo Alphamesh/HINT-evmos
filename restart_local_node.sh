fc8ccf6dd9275501613be242c3077be1da2f4aaa
RS <liu6rensheng@gmail.com>
2023-09-19 18:09:58 +0800

    Update: cd

--- restart_local_node.sh
+++ restart_local_node.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd /home/ubuntu/code/HINT-evmos
 ps aux | grep 'evmosd start' | grep -v grep|cut -c 9-16|xargs kill -15
 sleep 10
-/home/ubuntu/code/HINT-evmos/ubuntu_local_node.sh
+./ubuntu_local_node.sh
