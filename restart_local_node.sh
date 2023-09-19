6ea34fc80a6cc46e708d8fdb4b8e305ea6a9597b
RS <liu6rensheng@gmail.com>
2023-09-19 17:20:14 +0800

    Update: restart local node script

---
+++ restart_local_node.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ps aux | grep local_node | grep -v grep|cut -c 9-16|xargs kill -15
+ps aux | grep evmosd | grep -v grep|cut -c 9-16|xargs kill -15
+sleep 10
+/home/ubuntu/code/HINT-evmos/local_node.sh
